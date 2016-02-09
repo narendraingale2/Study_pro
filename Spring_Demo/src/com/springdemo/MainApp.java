73c36813090e700e455db0b2301178b769a1474a
Narendra <narendraingale2@gmail.com>
2016-02-09 13:01:13 +0530

    a

---
+++ Spring_Demo/src/com/springdemo/MainApp.java
@@ -0,0 +1,20 @@
+package com.springdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext context= new ClassPathXmlApplicationContext("Bean.xml");
+		
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+		
+		
+		
+	}
+
+}
